225b0afd60c915b1abdc9f54c1265e5ed4ad9dfd
milkbag <milkbag@milkbag.local>
2017-06-17 19:26:43 -0400

    stuff

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    happyBirthday()
+
+def happyBirthday():
+    print ("Happy Birthday")
+
+main()
+
+
+#API: AIzaSyB-09W17tZ08f4mfSG4LWaqSFvuqK8MgNY
+
+
+# what functions:
+# apiShit() - API key
+# callGoogle - query google
+# optimize -  what to do with response
+##### ^^^ deep learning
+# what to go with data
+# how to get USER data (input)
+# how to visualize results
