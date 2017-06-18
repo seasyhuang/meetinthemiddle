d78b9d60c1fc9dcd5064dbbf5da6ae4b1244a4a9
DerekYu <derek-nis@hotmail.com>
2017-06-18 13:51:32 -0400

    [Derek] removing main.py and turning this into a module that will be called in the future

--- data_generation.py
+++ data_generation.py
@@ -20,65 +20,24 @@ DISTANCE_TRAVELLED_IN_10_MINUTES = 833
 DAT_FILE = "scrape_stuff/raw.dat"
 MAX_LABELS = 100
 
-# gmaps = googlemaps.Client(key=API_KEY)
-# address = 'Constitution Ave NW & 10th St NW, Washington, DC'
-# loc = {u'lat': 45.501689, u'lng': -73.567256}
-#
-# dict = gmaps.places_nearby(location=loc, radius = 2000, type='parking')
-#
-# for key in dict['results']:
-#     latitude = key['geometry']['location']['lat']
-#     longitude = key['geometry']['location']['lng']
-#
-#     print key['name']+ " at location of " + str(latitude) + ", " + str(
-#         longitude)+'at distance '
-#
-# amusement_park accounting airport aquarium art_gallery atm bakery bank bar beauty_salon bicycle_store book_store
-# bowling_alley bus_station cafe campground car_dealer car_rental car_repair car_wash casino cemetery church city_hall
-# clothing_store convenience_store courthouse dentist department_store doctor electrician electronics_store embassy
-# establishment  finance  fire_station florist food  funeral_home furniture_store
-# gas_station general_contractor  grocery_or_supermarket  gym hair_care hardware_store health
-# hindu_temple home_goods_store hospital insurance_agency jewelry_store laundry lawyer library liquor_store local_government_office
-# locksmith lodging meal_delivery meal_takeaway mosque movie_rental movie_theater moving_company museum night_club painter park
-# parking pet_store pharmacy physiotherapist place_of_worship  plumber police post_office real_estate_agency restaurant
-# roofing_contractor rv_park school shoe_store shopping_mall spa stadium storage store subway_station synagogue taxi_stand
-# train_station transit_station travel_agency university veterinary_care zoo
-#
-# print (dict)
-# lat, lng = gmaps.address_to_latlng(address)
-# print lat, lng
-# 38.8921021 -77.0260358
-# Reverse Geocoding: find the nearest address to (lat, lng)
+# ------------------------------------------------- DATA CALL ---------------------------------------------
 
-# destination = gmaps.latlng_to_address(38.887563, -77.019929)
-# print destination
-# Independence and 6th SW, Washington, DC 20024, USA
-# Local Search: find places matching a query near a given location
+#CALL THIS
+def create_label_with_points():
+    # returns array of size 100
+    file = open_dat_file()
 
-# local = gmaps.local_search('cafe near ' + destination)
-# print local['responseData']['results'][0]['titleNoFormatting']
-# Vie De France Bakery & Cafe
+    data = numpy.array
 
+    count = 0
+    for midpoints in file:
 
-def main():
-    # moveMidpoint(111, 2222, 333333)
-    test_origin = "75 9th Ave New York, NY"
-    test_origin2 = "75 10th Ave New York, NY"
-    test_origin3 = "75 11th Ave New York, NY"
-    test_destination = "1124 Robie St Halifax, NS"
+        numpy.append(data, generate_element_in_row(midpoints))
 
-    # print distanceMatrix(test_origin, test_origin2, test_origin3, test_destination)
-    test_coordinate = "45.504168, -73.575524"
-    x = "45.504168"
-    y = "-73.575524"
-    print reverse_geocode_lookup(x, y)
+        count++
+        break if count >= MAX_LABELS
 
-
-
-def moveMidpoint(t1, t2, t3):
-    # input 3 times: check to see which one is largest
-    times = [t1, t2, t3]
-    largestTime = max(times) # --> want to move the midpoint toward respective person
+    return data
 
 # ------------------------------------------------- MIDPOINT CALCULATION ---------------------------------------------
 
@@ -163,22 +122,6 @@ def open_dat_file():
 
 # ------------------------------------------------- CREATING LABEL ARRAY ---------------------------------------------
 
-def create_label_with_points():
-    # returns array of size 100
-    file = open_dat_file()
-
-    data = numpy.array
-
-    count = 0
-    for midpoints in file:
-
-        numpy.append(data, generate_element_in_row(midpoints))
-
-        count++
-        break if count >= MAX_LABELS
-
-    return data
-
 def generate_element_in_row(midpoint):
     # takes the form = [(label_x, label_y), (rand_x1, rand_y1), (rand_x2, rand_y2), (rand_x3, rand_y3)]
     label_x, label_y = find_x_y(midpoint)
@@ -200,5 +143,3 @@ def triple_points(midpoint):
         triple_x.append((x_x, x_y))
 
     return triple_x
-
-main()
